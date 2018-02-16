d5ac4805d393032ae3ff5efb5f90f205b4bd1c06
Frank_probando_validacion <MLA01@MLA01>
2018-02-16 12:56:45 -0600

    modificaciones_de_las_vistas_de _humo_ Prueba de validacion...

--- Proyectores/Models/Departamento.cs
+++ Proyectores/Models/Departamento.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,16 +11,22 @@ namespace Proyectores.Models
     {
         [Key]
         public int id_departamento { get; set; }
+
         [Required(ErrorMessage = " Complete!!! Nombre del Departamento?")]
         [DataType(DataType.Text)]
         [StringLength(100, MinimumLength = 7)]
-        [Display(Name = "Nombre del Departamento")]
-        public string ConfirmNewPassword { get; set; }
+        [Display (Name ="Departamento")]        
+       // public string ConfirmNewPassword { get; set; }
         public string nombre { get; set; }
+        [Display(Name = "Misión")]
         public string mision { get; set; }
+        [Display(Name = "Visión")]
         public string vision { get; set; }
+        [Display(Name = "Historia")]
         public string historia { get; set; }
+        [Display(Name = "Ubicación")]
         public string ubicacion { get; set; }
+        [Display(Name = "Organización")]
         public string organizacion { get; set; }
 
         public override string ToString()
