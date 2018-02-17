e9751a8a86022fcc7bf956edafac5c58c562eccb
altair <altair@DESKTOP>
2018-02-17 14:03:34 -0600

    dsf

--- Proyectores/App_Start/BundleConfig.cs
+++ Proyectores/App_Start/BundleConfig.cs
@@ -9,8 +9,9 @@ namespace Proyectores
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        //"~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui-{version}.js"));
+                       "~/Scripts/jquery-{version}.js",
+                       "~/Scripts/jquery-ui-{version}.js"))
+                        ;
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios. De este modo, estará
             // preparado para la producción y podrá utilizar la herramienta de compilación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
@@ -18,10 +19,10 @@ namespace Proyectores
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                     // "~/Scripts/bootstrap.js",
                       "~/Scripts/light-bootstrap-dashboard.js",
-                      "~/Scripts/bootstrap-notify.js",
-                      "~/Scripts/bootstrap-select.js",
+                      //"~/Scripts/bootstrap-notify.js",
+                      //"~/Scripts/bootstrap-select.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
